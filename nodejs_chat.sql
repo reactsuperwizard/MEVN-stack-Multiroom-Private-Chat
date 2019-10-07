2ef639ac865c20854288e5e346ac46336f58cf51
Acodewizard <toprated0128@outlook.com>
2019-10-08 00:08:34 +0800

    update nodejs sql file

--- nodejs_chat.sql
+++ nodejs_chat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 04, 2019 at 05:39 PM
+-- Generation Time: Oct 07, 2019 at 06:07 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -21,7 +21,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `nodejs_chat`
 --
-CREATE DATABASE IF NOT EXISTS `nodejs_chat` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE IF NOT EXISTS `nodejs_chat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE `nodejs_chat`;
 
 -- --------------------------------------------------------
@@ -29,74 +29,57 @@ USE `nodejs_chat`;
 --
 -- Table structure for table `messages`
 --
+-- Creation: Oct 06, 2019 at 07:33 PM
+--
 
 DROP TABLE IF EXISTS `messages`;
-CREATE TABLE `messages` (
-  `id` int(11) NOT NULL,
-  `content` varchar(1028) NOT NULL,
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `content` varchar(1028) COLLATE utf8mb4_bin NOT NULL,
   `room` int(11) NOT NULL,
   `user` int(11) DEFAULT NULL,
   `admin` tinyint(1) NOT NULL DEFAULT '0',
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=625 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 --
 -- Dumping data for table `messages`
 --
 
 INSERT INTO `messages` (`id`, `content`, `room`, `user`, `admin`, `createdAt`, `updatedAt`) VALUES
-(297, '1user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:24:17', '2019-10-04 14:24:17'),
-(298, '2user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:24:21', '2019-10-04 14:24:21'),
-(299, '3user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:24:25', '2019-10-04 14:24:25'),
-(300, 'Hi there', 19, 52, 0, '2019-10-04 14:24:35', '2019-10-04 14:24:35'),
-(301, 'Yes', 19, 53, 0, '2019-10-04 14:24:40', '2019-10-04 14:24:40'),
-(302, 'I can see you all.', 19, 54, 0, '2019-10-04 14:24:51', '2019-10-04 14:24:51'),
-(303, 'great', 19, 52, 0, '2019-10-04 14:24:56', '2019-10-04 14:24:56'),
-(304, '1user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:25:14', '2019-10-04 14:25:14'),
-(305, '222', 19, 52, 0, '2019-10-04 14:25:19', '2019-10-04 14:25:19'),
-(306, '2user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:25:21', '2019-10-04 14:25:21'),
-(307, '3user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:25:23', '2019-10-04 14:25:23'),
-(308, 'abc', 19, 52, 0, '2019-10-04 14:25:24', '2019-10-04 14:25:24'),
-(309, '222', 19, 53, 0, '2019-10-04 14:25:27', '2019-10-04 14:25:27'),
-(310, '1user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:25:32', '2019-10-04 14:25:32'),
-(311, 'aaa', 20, 52, 0, '2019-10-04 14:25:34', '2019-10-04 14:25:34'),
-(312, 'abc', 20, 52, 0, '2019-10-04 14:25:36', '2019-10-04 14:25:36'),
-(313, 'ddd', 20, 52, 0, '2019-10-04 14:25:38', '2019-10-04 14:25:38'),
-(314, 'abc', 19, 53, 0, '2019-10-04 14:25:41', '2019-10-04 14:25:41'),
-(315, '3434', 19, 54, 0, '2019-10-04 14:25:44', '2019-10-04 14:25:44'),
-(316, '2user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:25:51', '2019-10-04 14:25:51'),
-(317, '3user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:25:53', '2019-10-04 14:25:53'),
-(318, '32424', 20, 52, 0, '2019-10-04 14:25:56', '2019-10-04 14:25:56'),
-(319, 'abc', 20, 53, 0, '2019-10-04 14:25:58', '2019-10-04 14:25:58'),
-(320, '3434', 20, 54, 0, '2019-10-04 14:25:59', '2019-10-04 14:25:59'),
-(321, 'abc', 20, 52, 0, '2019-10-04 14:27:21', '2019-10-04 14:27:21'),
-(322, '1user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:27:23', '2019-10-04 14:27:23'),
-(323, '2user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:27:24', '2019-10-04 14:27:24'),
-(324, '3user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:27:25', '2019-10-04 14:27:25'),
-(325, 'asd', 20, 52, 0, '2019-10-04 14:27:27', '2019-10-04 14:27:27'),
-(326, 'asd', 20, 53, 0, '2019-10-04 14:27:29', '2019-10-04 14:27:29'),
-(327, 'abc', 20, 54, 0, '2019-10-04 14:27:30', '2019-10-04 14:27:30'),
-(328, '1user left 1user\'s second room', 20, NULL, 1, '2019-10-04 14:27:32', '2019-10-04 14:27:32'),
-(329, '1user joined 1user\'s second room', 20, NULL, 1, '2019-10-04 14:27:43', '2019-10-04 14:27:43'),
-(330, '222', 20, 52, 0, '2019-10-04 14:27:46', '2019-10-04 14:27:46'),
-(331, 'abc', 20, 53, 0, '2019-10-04 14:27:49', '2019-10-04 14:27:49'),
-(332, '4334', 20, 54, 0, '2019-10-04 14:27:51', '2019-10-04 14:27:51'),
-(333, '1user left 1user\'s second room', 20, NULL, 1, '2019-10-04 14:28:03', '2019-10-04 14:28:03'),
-(334, '3user left 1user\'s second room', 20, NULL, 1, '2019-10-04 14:34:46', '2019-10-04 14:34:46'),
-(335, '2user left 1user\'s second room', 20, NULL, 1, '2019-10-04 14:35:00', '2019-10-04 14:35:00'),
-(336, '1user joined 1user\'s room', 19, NULL, 1, '2019-10-04 14:36:16', '2019-10-04 14:36:16'),
-(337, '1user left 1user\'s room', 19, NULL, 1, '2019-10-04 14:36:38', '2019-10-04 14:36:38');
+(604, '1user joined 1user\'s room', 24, NULL, 1, '2019-10-07 15:23:21', '2019-10-07 15:23:21'),
+(605, '2user joined 1user\'s room', 24, NULL, 1, '2019-10-07 15:23:22', '2019-10-07 15:23:22'),
+(606, '1user joined 1user\'s room', 24, NULL, 1, '2019-10-07 15:24:55', '2019-10-07 15:24:55'),
+(607, '2user joined 1user\'s room', 24, NULL, 1, '2019-10-07 15:25:00', '2019-10-07 15:25:00'),
+(608, 'Hi there😌', 24, 57, 0, '2019-10-07 15:25:10', '2019-10-07 15:25:10'),
+(609, 'I can see.🤑', 24, 58, 0, '2019-10-07 15:27:13', '2019-10-07 15:27:13'),
+(612, '1user joined 1user\'s room', 24, NULL, 1, '2019-10-07 15:29:59', '2019-10-07 15:29:59'),
+(613, '1user joined 1user\'s room', 24, NULL, 1, '2019-10-07 16:02:13', '2019-10-07 16:02:13'),
+(614, '1user left 1user\'s room', 24, NULL, 1, '2019-10-07 16:02:40', '2019-10-07 16:02:40'),
+(615, '1user joined 1user\'s second room', 25, NULL, 1, '2019-10-07 16:03:38', '2019-10-07 16:03:38'),
+(616, 'haha😛', 25, 57, 0, '2019-10-07 16:03:44', '2019-10-07 16:03:44'),
+(617, '2user left 1user\'s room', 24, NULL, 1, '2019-10-07 16:03:51', '2019-10-07 16:03:51'),
+(618, '2user joined 1user\'s room', 24, NULL, 1, '2019-10-07 16:03:55', '2019-10-07 16:03:55'),
+(619, '2user left 1user\'s room', 24, NULL, 1, '2019-10-07 16:03:56', '2019-10-07 16:03:56'),
+(620, '2user joined 1user\'s second room', 25, NULL, 1, '2019-10-07 16:03:59', '2019-10-07 16:03:59'),
+(621, '2user joined 1user\'s second room', 25, NULL, 1, '2019-10-07 16:04:11', '2019-10-07 16:04:11'),
+(622, 'qqq🤡', 25, 58, 0, '2019-10-07 16:04:17', '2019-10-07 16:04:17'),
+(623, '2user left 1user\'s second room', 25, NULL, 1, '2019-10-07 16:04:22', '2019-10-07 16:04:22'),
+(624, '1user left 1user\'s second room', 25, NULL, 1, '2019-10-07 16:04:26', '2019-10-07 16:04:26');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `rooms`
 --
+-- Creation: Sep 30, 2019 at 02:24 PM
+--
 
 DROP TABLE IF EXISTS `rooms`;
-CREATE TABLE `rooms` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `rooms` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   `user` varchar(11) DEFAULT NULL,
   `password` varchar(50) DEFAULT '',
@@ -104,28 +87,29 @@ CREATE TABLE `rooms` (
   `accessIds` varchar(250) DEFAULT NULL,
   `users` varchar(250) DEFAULT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `rooms`
 --
 
 INSERT INTO `rooms` (`id`, `name`, `user`, `password`, `access`, `accessIds`, `users`, `createdAt`, `updatedAt`) VALUES
-(19, '1user\'s room', '52', NULL, 1, NULL, NULL, '2019-10-04 14:21:16', '2019-10-04 14:21:16'),
-(20, '1user\'s second room', '52', NULL, 1, NULL, NULL, '2019-10-04 14:21:30', '2019-10-04 14:21:30'),
-(21, '2user\'s room', '53', NULL, 1, NULL, NULL, '2019-10-04 14:21:51', '2019-10-04 14:21:51'),
-(22, '3user\'s room', '54', NULL, 1, NULL, NULL, '2019-10-04 14:23:54', '2019-10-04 14:23:54');
+(24, '1user\'s room', '57', NULL, 1, NULL, NULL, '2019-10-07 15:23:18', '2019-10-07 15:23:18'),
+(25, '1user\'s second room', '57', NULL, 1, NULL, NULL, '2019-10-07 16:02:52', '2019-10-07 16:02:52');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `user_infos`
 --
+-- Creation: Oct 03, 2019 at 02:38 AM
+--
 
 DROP TABLE IF EXISTS `user_infos`;
-CREATE TABLE `user_infos` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `user_infos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `socketid` char(255) DEFAULT NULL,
   `username` varchar(20) NOT NULL,
   `handle` varchar(50) NOT NULL,
@@ -142,61 +126,17 @@ CREATE TABLE `user_infos` (
   `status_participate` int(11) NOT NULL DEFAULT '0',
   `room_id` int(11) NOT NULL,
   `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user_infos`
 --
 
 INSERT INTO `user_infos` (`id`, `socketid`, `username`, `handle`, `email`, `password`, `create_time`, `delete_time`, `image`, `age`, `sex`, `location`, `bio`, `status_active`, `status_participate`, `room_id`, `createdAt`, `updatedAt`) VALUES
-(52, '1yiiwSupZj9LFUQpAAAD', '1user', '1user', '1user@email.com', '$2a$10$vV1bt.0A0uOKPMHXqmWs5.ciMIpMxuHRD.Y2ZvlkJxEJk9hzi2b0.', '2019-10-03 04:29:45', NULL, '//www.gravatar.com/avatar/52136435f4a0135ba0f4e8cf78fdd095?s=220&r=pg&d=identicon', NULL, NULL, '', '', 0, 0, 0, '2019-10-04 14:36:38', '2019-10-04 14:36:38'),
-(53, 'uotIxBKw8VNiXTuHAAAE', '2user', '2user', '2user@email.com', '$2a$10$0jVamfbJWdoL.OM0cC8.F.T9fg5dOUre.dW37goSK5kahDBc3bCNK', '2019-10-03 04:39:36', NULL, '//www.gravatar.com/avatar/d920d7080934a66a52a5d2c6c8182376?s=220&r=pg&d=identicon', NULL, NULL, '', '', 0, 0, 0, '2019-10-04 15:34:28', '2019-10-04 15:34:28'),
-(54, 'QS5lgDzSVZU9W1UKAAAF', '3user', '3user', '3user@email.com', '$2a$10$lwbtwJjrZUqw09VF5A2GdO.ZW9MAg0ZFn2qe5mlsTfLPiPba0Wi5G', '2019-10-03 14:31:45', NULL, 'images (1).jpg', 23, 'female', 'user3\'s house', 'I live in user3\'s house!!!', 0, 0, 0, '2019-10-04 14:34:46', '2019-10-04 14:34:46');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `rooms`
---
-ALTER TABLE `rooms`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `user_infos`
---
-ALTER TABLE `user_infos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=338;
-
---
--- AUTO_INCREMENT for table `rooms`
---
-ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `user_infos`
---
-ALTER TABLE `user_infos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
+(57, 'U7PgBL6N9an9kY18AAAC', '1user', '1user', '1user@email.com', '$2a$10$kWepNTGXCRJOnyUR7BiQKOm7PGHUnjrjSlPc0OUDgUTJVmS.FqiRS', '2019-10-07 15:21:35', NULL, 'db1afb1c-509a-435b-8a53-1f25d7af101c.jpg', 19, 'female', 'I am a girl.', 'I am a 19 years old girl.', 0, 0, 0, '2019-10-07 16:04:26', '2019-10-07 16:04:26'),
+(58, 'O0xXiLdHkZinNKe8AAAD', '2user', '2user', '2user@email.com', '$2a$10$i3eEEiWCWQ3OpiZTnmIBd.31JQDg4O8F4Y7MrYU7YlhMJD9PT03Ti', '2019-10-07 15:21:41', NULL, '3ca17dba-78c4-464b-9858-1c4c9948744c.jpg', 22, 'male', 'I am a man.', 'I am a man.\nI am 22 years old.', 0, 0, 0, '2019-10-07 16:04:22', '2019-10-07 16:04:22');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
