b375bac0b16f0f9c6556655294d4f34374bb95aa
Acodewizard <toprated0128@outlook.com>
2019-11-12 22:25:00 +0800

    delete room after an hour

---
+++ server/actions/cronjob.js
@@ -0,0 +1,28 @@
+const Sequelize = require('sequelize');
+const Op = Sequelize.Op;
+const {
+    DELETE_ROOM_BY_PARAM
+} = require('./socketio');
+const Room = require('../models/Room');
+
+module.exports = {
+    DELETE_STICKY_ROOM: async () => {
+        console.log('DELETE STICKY ROOM');
+        const curr = new Date();
+        const rooms = await Room.findAll({
+            where: {
+                lastAcTime: {
+                    [Op.ne]: null
+                }
+            }
+        }, {
+            raw: true
+        })
+        rooms.forEach(room => {
+            const befr = new Date(room.lastAcTime);
+            if (curr.getTime() - befr.getTime() > 60 * 60 * 1000) {
+                DELETE_ROOM_BY_PARAM(room);
+            }
+        })
+    }
+};
