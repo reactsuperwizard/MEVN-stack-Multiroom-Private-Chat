9dd9e706ea7b0b9791280d226cb7cec898e4e3a2
Acodewizard <toprated0128@outlook.com>
2019-11-13 22:27:51 +0800

    delete account emit update user list

--- client/components/user/UserProfile.vue
+++ client/components/user/UserProfile.vue
@@ -5,12 +5,20 @@
 				<span class="section__title">Dashboard</span>
 			</div>
 			<div class="section__content">
-				<router-link :to="{name: 'RoomList'}" class="btn btn--info mt-3 center">Start Chatting!</router-link>
+				<router-link
+					:to="{ name: 'RoomList' }"
+					class="btn btn--info mt-3 center"
+					>Start Chatting!</router-link
+				>
 				<div class="infobox__container" v-if="user">
 					<span class="lead">Your current profile</span>
 					<div class="infobox__item">
 						<img
-							:src="(!user.image.includes('www.gravatar.com/avatar') ? 'http://localhost:5000/public/avatar/' : '') + user.image"
+							:src="
+								(!user.image.includes('www.gravatar.com/avatar')
+									? 'http://localhost:5000/public/avatar/'
+									: '') + user.image
+							"
 							alt
 							class="profile__image"
 						/>
@@ -33,26 +41,42 @@
 					</div>
 					<div class="infobox__item">
 						<span class="infobox__item--left">Age</span>
-						<span class="infobox__item--right">{{ user.age || 'Unknown' }}</span>
+						<span class="infobox__item--right">{{
+							user.age || 'Unknown'
+						}}</span>
 					</div>
 					<div class="infobox__item">
 						<span class="infobox__item--left">Gender</span>
-						<span class="infobox__item--right">{{ user.sex || 'Unknown' }}</span>
+						<span class="infobox__item--right">{{
+							user.sex || 'Unknown'
+						}}</span>
 					</div>
 					<div class="infobox__item">
 						<span class="infobox__item--left">Location</span>
-						<span class="infobox__item--right">{{ user.location || 'Unknown' }}</span>
+						<span class="infobox__item--right">{{
+							user.location || 'Unknown'
+						}}</span>
 					</div>
 					<div class="infobox__item">
 						<span class="infobox__item--left">Bio</span>
-						<textarea class="infobox__item--right" v-model="user.bio" disabled></textarea>
+						<textarea
+							class="infobox__item--right"
+							v-model="user.bio"
+							disabled
+						></textarea>
 					</div>
 					<div class="infobox__actions mt-3" v-if="user">
 						<router-link
-							:to="{name: 'EditUserProfile', params: { handle: user.handle }}"
+							:to="{ name: 'EditUserProfile', params: { handle: user.handle } }"
 							class="btn btn--info"
-						>Edit Profile</router-link>
-						<a href="#" @click.prevent="handleDeleteModal" class="btn btn--danger">Delete Account</a>
+							>Edit Profile</router-link
+						>
+						<a
+							href="#"
+							@click.prevent="handleDeleteModal"
+							class="btn btn--danger"
+							>Delete Account</a
+						>
 					</div>
 				</div>
 				<Modal name="deleteUser" ref="deleteUser">
@@ -61,9 +85,13 @@
 					</template>
 					<template slot="body">
 						<p class="lead">Warning: This action cannot be undone</p>
-						<p class="lead mt-6">Are you sure you want to permanently delete your account?</p>
+						<p class="lead mt-6">
+							Are you sure you want to permanently delete your account?
+						</p>
 						<div class="actions mt-6">
-							<a href="#" @click.prevent="handleDelete" class="btn btn--danger">Yes, Delete Account</a>
+							<a href="#" @click.prevent="handleDelete" class="btn btn--danger"
+								>Yes, Delete Account</a
+							>
 						</div>
 					</template>
 				</Modal>
@@ -73,53 +101,53 @@
 </template>
 
 <script>
-	import axios from "axios";
-	import _ from "lodash";
-	import { mapActions, mapGetters } from "vuex";
-	import Modal from "@/components/layout/Modal.vue";
+import axios from 'axios';
+import _ from 'lodash';
+import { mapActions, mapGetters } from 'vuex';
+import Modal from '@/components/layout/Modal.vue';
 
-	export default {
-		name: "UserProfile",
-		components: {
-			Modal
+export default {
+	name: 'UserProfile',
+	components: {
+		Modal
+	},
+	data: function() {
+		return {
+			user: null
+		};
+	},
+	computed: {
+		...mapGetters(['getUserData', 'isAuthorized', 'getSocket'])
+	},
+	methods: {
+		...mapActions(['saveUserData', 'toggleAuthState', 'deleteUserAccount']),
+		handleDeleteModal() {
+			this.$refs.deleteUser.open();
 		},
-		data: function() {
-			return {
-				user: null
-			};
-		},
-		computed: {
-			...mapGetters(["getUserData", "isAuthorized"])
-		},
-		methods: {
-			...mapActions(["saveUserData", "toggleAuthState", "deleteUserAccount"]),
-			handleDeleteModal() {
-				this.$refs.deleteUser.open();
-			},
-			handleDelete() {
-				this.$refs.deleteUser.close();
-				this.$store.dispatch("deleteUserAccount");
-			}
-		},
-		created() {
-			if (localStorage.getItem("authToken") && _.isEmpty(this.getUserData)) {
-				axios
-					.get(`/api/user/current`)
-					.then(res => {
-						this.$store.dispatch("saveUserData", res.data);
-						this.$store.dispatch("toggleAuthState", true);
-						this.user = res.data;
-					})
-					.catch(err => err);
-			} else {
-				this.user = this.getUserData;
-			}
+		handleDelete() {
+			this.$refs.deleteUser.close();
+			this.$store.dispatch('deleteUserAccount');
+			this.getSocket.emit('UserRegistered', '');
 		}
-	};
+	},
+	created() {
+		if (localStorage.getItem('authToken') && _.isEmpty(this.getUserData)) {
+			axios
+				.get(`/api/user/current`)
+				.then(res => {
+					this.$store.dispatch('saveUserData', res.data);
+					this.$store.dispatch('toggleAuthState', true);
+					this.user = res.data;
+				})
+				.catch(err => err);
+		} else {
+			this.user = this.getUserData;
+		}
+	}
+};
 </script>
 
-
 <style lang="scss">
-	@import "@/assets/scss/views/profile.scss";
-	@import "@/assets/scss/components/infobox.scss";
+@import '@/assets/scss/views/profile.scss';
+@import '@/assets/scss/components/infobox.scss';
 </style>
