35825b7d9abf945ee77fe5c43c02f697be4840bf
Acodewizard <toprated0128@outlook.com>
2019-12-08 22:49:38 +0800

    delete unnecessary packages

--- nodejs_chat.sql
+++ nodejs_chat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2019 at 07:34 PM
+-- Generation Time: Dec 08, 2019 at 03:43 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.12
 
@@ -23,6 +23,30 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `nodejs_chat` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 USE `nodejs_chat`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adsenses`
+--
+
+DROP TABLE IF EXISTS `adsenses`;
+CREATE TABLE `adsenses` (
+  `id` int(11) NOT NULL,
+  `chatInputClientId` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `chatInputSlotId` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `sidebarClientId` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `sidebarSlotId` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `updatedAt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Dumping data for table `adsenses`
+--
+
+INSERT INTO `adsenses` (`id`, `chatInputClientId`, `chatInputSlotId`, `sidebarClientId`, `sidebarSlotId`, `createdAt`, `updatedAt`) VALUES
+(1, '4125889001430424', '5349526397', '4125889001430424', '5349526397', '2019-12-08 14:14:57', '0000-00-00 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -72,6 +96,16 @@ CREATE TABLE `relations` (
   `status` int(11) NOT NULL DEFAULT '2'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+--
+-- Dumping data for table `relations`
+--
+
+INSERT INTO `relations` (`id`, `user`, `touser`, `status`) VALUES
+(9, 3, 1, 0),
+(10, 2, 1, 0),
+(11, 3, 2, 0),
+(12, 1, 2, 0);
+
 -- --------------------------------------------------------
 
 --
@@ -98,7 +132,7 @@ CREATE TABLE `rooms` (
 
 INSERT INTO `rooms` (`id`, `name`, `avatar`, `user`, `password`, `access`, `lastAcTime`, `users`, `createdAt`, `updatedAt`) VALUES
 (1, 'Private Room', 'pHome.png', NULL, NULL, 0, NULL, NULL, '2019-11-11 06:17:18', '2019-10-09 06:40:18'),
-(2, 'HOME', 'home.png', 'NULL', NULL, 1, NULL, NULL, '2019-11-25 13:09:01', '2019-11-25 13:09:00');
+(2, 'HOME', 'home.png', 'NULL', NULL, 1, NULL, NULL, '2019-12-08 14:39:02', '2019-12-08 14:39:02');
 
 -- --------------------------------------------------------
 
@@ -143,8 +177,24 @@ CREATE TABLE `user_infos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `user_infos`
+--
+
+INSERT INTO `user_infos` (`id`, `socketid`, `username`, `handle`, `email`, `password`, `create_time`, `delete_time`, `image`, `age`, `sex`, `location`, `bio`, `status_active`, `status_participate`, `room_id`, `createdAt`, `updatedAt`) VALUES
+(1, 'q1VZJxyym57L7weNAAAF', 'cat1', 'cat1', 'cat1@email.com', '$2a$10$sO7h/uIzDZx6GvbIVnEl9OWeZEH85.RqGHQMddLNzhr7dVtCjaWKW', '2019-12-07 15:04:28', NULL, '//www.gravatar.com/avatar/4fc3103a0027f9ad4e3f6f37debe8e52?s=220&r=pg&d=identicon', NULL, NULL, '', '', 1, 0, NULL, '2019-12-08 14:38:54', '2019-12-08 14:38:54'),
+(2, 'TZrt8AfJxMfaGij7AAAG', 'cat2', 'cat2', 'cat2@email.com', '$2a$10$jhECiO2BD4DmgXIdwKO9t.GbTawjX7ka2ZwasuQdr.8WRJKGKZdty', '2019-12-07 15:04:36', NULL, '//www.gravatar.com/avatar/6312d7776c273231bb2743d1e3ee952c?s=220&r=pg&d=identicon', NULL, NULL, '', '', 1, 0, 2, '2019-12-08 14:39:02', '2019-12-08 14:39:02'),
+(3, 'JUA9EVf7v0rWvYn2AAAH', 'cat3', 'cat3', 'cat3@a.com', '$2a$10$0wIfQl0igwWP.HcS.3Pn0eaCSquaT8nrV74yaUyI6zF2SRFZOwAfy', '2019-12-07 15:09:06', NULL, '//www.gravatar.com/avatar/f12f252dc48684c3ff33367291bef7e0?s=220&r=pg&d=identicon', NULL, NULL, '', '', 1, 0, NULL, '2019-12-08 14:38:53', '2019-12-08 14:38:53');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `adsenses`
+--
+ALTER TABLE `adsenses`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`);
 
 --
 -- Indexes for table `messages`
@@ -191,28 +241,34 @@ ALTER TABLE `user_infos`
 --
 
 --
+-- AUTO_INCREMENT for table `adsenses`
+--
+ALTER TABLE `adsenses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=198;
 
 --
 -- AUTO_INCREMENT for table `privatemessages`
 --
 ALTER TABLE `privatemessages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- AUTO_INCREMENT for table `relations`
 --
 ALTER TABLE `relations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10055;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `room_relations`
@@ -224,7 +280,7 @@ ALTER TABLE `room_relations`
 -- AUTO_INCREMENT for table `user_infos`
 --
 ALTER TABLE `user_infos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
